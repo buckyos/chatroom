b00a6bb76e3b800993cb0cfb926122cbf8384841
waterflier <waterflier@gmail.com>
2017-02-28 17:14:42 +0800

    完善脚本提示。

--- update_latest_sdk.bat
+++ update_latest_sdk.bat
@@ -1,5 +1,9 @@
+@echo off
+echo downloading latest sdk from github....
 node download_sdk.js
 del /q master
+
+echo extracing sdk...
 
 xcopy /Y .\bucky_sdk-master\proxytools.js .\
 xcopy /Y .\bucky_sdk-master\tools.js .\
@@ -9,3 +13,5 @@ mkdir .\source\target\wx\bucky
 xcopy /Y .\bucky_sdk-master\wx_core.js .\source\target\wx\bucky\
 
 rd /s /q .\bucky_sdk-master
+
+echo update complete.
--- update_latest_sdk.sh
+++ update_latest_sdk.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+echo downloading latest sdk from github....
 node download_sdk.js
 rm -f master
+
+echo extracing sdk...
 
 cp -f ./bucky_sdk-master/proxytools.js ./
 cp -f ./bucky_sdk-master/tools.js ./
@@ -11,3 +14,5 @@ mkdir -p ./source/target/wx/bucky
 cp -f ./bucky_sdk-master/wx_core.js ./source/target/wx/bucky/
 
 rm -rf ./bucky_sdk-master
+
+echo update complete.
